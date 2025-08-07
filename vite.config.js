c25fb938c492c6a676c9a87c0e3d2c4d90848e01
kevin santiago rodriguez gomez <kevin.rodriguezgomez1308@gmail.com>
2025-08-07 10:59:58 -0500

    solución de errores al compilar

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuración para Vite. Usa el plugin de React para habilitar JSX y Fast Refresh.
 export default defineConfig({
+  root: '.', // 👈 necesario para que Vite encuentre public/index.html
   plugins: [react()],
   server: {
     port: 5173,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
   },
+  build: {
+    outDir: 'dist', // por si lo necesitas explícito
+    emptyOutDir: true
+  }
 });
