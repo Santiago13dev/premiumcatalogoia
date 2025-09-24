49f4c2bb5fa9f5215917e43d951b1ed2bed97bad
Kevin Rodriguez <kevin.rodriguezgomez1308@gmail.com>
2025-09-23 19:36:17 -0500

    fix postcss configuration error and move dependencies to correct section

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Punto de entrada de la aplicación React. Se adjunta al div con id 'root'.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
